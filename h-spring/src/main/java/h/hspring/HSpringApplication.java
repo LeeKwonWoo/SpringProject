f0614f95765ce532d94e86555ecb8ac639446752
LeeKwonWoo <kenu12@naver.com>
2023-04-20 15:07:19 +0900

    springproject

---
+++ h-spring/src/main/java/h/hspring/HSpringApplication.java
@@ -0,0 +1,13 @@
+package h.hspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSpringApplication.class, args);
+	}
+
+}
---
+++ h-spring/src/test/java/h/hspring/HSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package h.hspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
